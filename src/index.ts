46c2bcad93c7d5815c75e5c14babd1ee549963fa
fifteenmania <ktthee1995@gmail.com>
2021-12-21 11:54:02 +0900

    refactor: export all functions from gameUtil.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-export {getCurrentNum, getCurrentPlayer, getLastPlayer, handleAiTurns, handlePlayerTurn, gameIsEnd} from './lib/gameUtil'
+export * from './lib/gameUtil'
 export {PlayLog, PlayLogEntry} from './lib/gameUtil'
 
 export {getFullLoseProbMat} from './lib/strategy'
